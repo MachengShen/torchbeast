2d5031152de28a1d0188760e02b409b3356e3ee9
macheng <macshen@mit.edu>
2020-09-17 17:32:40 -0400

    add ubuntu mujoco dependency

--- multiagent-competition/env_setup.sh
+++ multiagent-competition/env_setup.sh
@@ -3,6 +3,7 @@
 # 'source env_setup.sh'
 #conda create -n comp_env python=3.6
 #conda activate comp_env
+sudo apt install libosmesa6-dev libgl1-mesa-glx libglfw3
 python3.6 -m pip install -r requirements.txt
 cd gym-compete
 python3.6 -m pip install -e .
